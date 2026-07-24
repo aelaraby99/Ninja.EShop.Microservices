317aa4fa1a253a9d26f383102fda32405792fc29
Ahmed Elaraby <ahme.elaraby@asset.com.eg>
2026-07-24 17:50:09 +0300

    Extend Discount gRPC service and refactor CRUD logic
    Added GetDiscountList RPC to discount.proto and updated field naming to camelCase. Refactored DiscountService to use DI, async EF Core operations, and improved logging and error handling. Updated .csproj with shared project reference and adjusted logging level in appsettings.json. Updated database binary files.

--- src/NINJA.EShop.Discount/NINJA.EShop.Discount.Grpc/Services/DiscountService.cs
+++ src/NINJA.EShop.Discount/NINJA.EShop.Discount.Grpc/Services/DiscountService.cs
@@ -1,20 +1,65 @@
 ﻿using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using NINJA.EShop.Discount.Grpc.Data;
 using NINJA.EShop.Discount.Grpc.Protos;
 namespace NINJA.EShop.Discount.Grpc.Services
 {
-    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService(DiscountContext discount,ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request,ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request,ServerCallContext context)
         {
-            return base.GetDiscount(request,context);
+            var coupon = await discount
+                .Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+            if (coupon == null)
+                return new CouponModel
+                {
+                    ProductName = request.ProductName,
+                    Description = "No discount available",
+                    Amount = 0
+                };
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
-        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,ServerCallContext context)
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,ServerCallContext context)
         {
-            return base.DeleteDiscount(request,context);
+            var coupon = await discount.Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.NotFound,$"Discount with ProductName={request.ProductName} is not found."));
+            discount.Coupons.Remove(coupon);
+            var result = await discount.SaveChangesAsync();
+            if (result > 0)
+                return new DeleteDiscountResponse { IsSuccess = true };
+            return new DeleteDiscountResponse { IsSuccess = false };
         }
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request,ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request,ServerCallContext context)
         {
-            return base.CreateDiscount(request,context);
+            var coupon = request.Coupon.Adapt<Models.Coupon>();
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument,"Invalid coupon data."));
+            discount.Coupons.Add(coupon);
+            await discount.SaveChangesAsync();
+            logger.LogInformation("Discount is successfully created. ProductName: {ProductName}",coupon.ProductName);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request,ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Models.Coupon>();
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument,"Invalid coupon data."));
+            discount.Coupons.Update(coupon);
+            await discount.SaveChangesAsync();
+            logger.LogInformation("Discount is successfully updated. ProductName: {ProductName}",coupon.ProductName);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+        public override async Task<GetDiscountListResponse> GetDiscountList(GetDiscountListRequest request,ServerCallContext context)
+        {
+            var coupons = await discount.Coupons.AsNoTracking().ToListAsync();
+            var copuponModels = coupons.Adapt<List<CouponModel>>();
+            return new GetDiscountListResponse { Coupons = { copuponModels } };
         }
     }
 }

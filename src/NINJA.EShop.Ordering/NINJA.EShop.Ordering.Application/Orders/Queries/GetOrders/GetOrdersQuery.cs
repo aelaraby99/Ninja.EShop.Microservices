d8678f4d912501cd9d73a6e13706cc7fc98b23a3
Ahmed Elaraby <ahme.elaraby@asset.com.eg>
2026-07-26 14:35:22 +0300

    Add pagination support to GetOrders query
    Implemented PaginationRequest and PaginatedResult in Shared.Pagination. Updated GetOrdersQuery and handler to support paginated order retrieval.

---
+++ src/NINJA.EShop.Ordering/NINJA.EShop.Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,5 @@
+﻿using NINJA.EShop.Shared.Pagination;
+namespace NINJA.EShop.Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest): IQuery<GetOrdersResult>;
+public record GetOrdersResult(PaginatedResult<OrderDto> Orders);
---
+++ src/NINJA.EShop.Shared/NINJA.EShop.Shared/Pagination/PaginatedResult.cs
@@ -0,0 +1,9 @@
+﻿namespace NINJA.EShop.Shared.Pagination;
+
+public class PaginatedResult<TEntity>(int pageSize,int pageIndex,long count,IEnumerable<TEntity> data) where TEntity : class
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pageSize;
+    public long Count { get; } = count;
+    public IEnumerable<TEntity> Data { get; } = data;
+}

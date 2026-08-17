8529fc17ba20939c17d1d3e1ac5aeb6151d76d32
Ahmed Elaraby <ahme.elaraby@asset.com.eg>
2026-08-17 21:24:49 +0300

    Add YARPAPI

---
+++ src/NINJA.EShop.APIGateway/NINJA.EShop.YARPApiGateway/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container.
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed",opt =>
+    {
+        opt.PermitLimit = 10;
+        opt.Window = TimeSpan.FromSeconds(10);
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseRateLimiter();
+app.MapReverseProxy();
+app.Run();
